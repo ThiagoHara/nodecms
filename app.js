1d00172299158d90c0c074a368707accc6f0c5ea
DESKTOP-CN8QNI8\Hara <rocha.skp@gmail.com>
2016-09-10 17:41:45 -0400

    altered app file

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('500', {
       message: err.message,
       error: err
     });
