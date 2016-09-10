7af5de5eaafdf64a8626e2b69ac187e9e4c1e4ca
T-HARA\hara <thiagomassamihara@gmail.com>
2016-09-10 19:14:12 -0400

    New route to 403, and admin page/route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/login', login);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +41,13 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
+//403
+app.use(function(req, res, next) {
+  var err = new Error('Forbbiden access');
+  err.status = 403;    
+  next(err);;
+});
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
